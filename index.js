7d0f58866f297052e3d7dc79647ea7727656ab28
Chris Foster <christophervfoster@gmail.com>
2022-01-22 12:10:59 -0800

    Adding 404 route

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 
@@ -5,7 +7,11 @@ app.get('/', (req, res) => {
     res.send('I AM SINNISTAR! BEWARE, I LIVE!')
 })
 
-app.listen(3000)
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>RUN, COWARD! I HUNGER!</h1>')
+})
+
+app.listen(process.env.PORT)
 
 
 //Adjusting code for test
