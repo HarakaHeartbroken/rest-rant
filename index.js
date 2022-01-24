36d9f92b789373b6da6658eea0717ccb93241b37
Chris Foster <christophervfoster@gmail.com>
2022-01-23 19:02:16 -0800

    Added README, adding remaining stub routes

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
-app.use('/places', require('/controllers/places'))
+app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
+    console.log("Test")
     res.send('I AM SINNISTAR! BEWARE, I LIVE!')
 })
 
@@ -12,7 +13,10 @@ app.get('*', (req, res) => {
     res.status(404).send('<h1>RUN, COWARD! I HUNGER!</h1>')
 })
 
-app.listen(process.env.PORT)
 
+// console.log("BEWARE, AM SINNISTAR")
+app.listen(process.env.PORT, function (){
+    console.log("I LIVE AGAIN!")
+})
 
 //Adjusting code for test
