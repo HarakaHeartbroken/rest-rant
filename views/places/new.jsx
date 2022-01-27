cdb0278b2c757f709f9f9cf55137aff497f1aaa6
Chris Foster <christophervfoster@gmail.com>
2022-01-26 20:29:57 -0800

    Jazzed up form page

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -6,6 +6,29 @@ function new_form () {
         <Def>
             <main>
                 <h1>Add A New Caster</h1>
+                <form method="POST" action="/places">
+                    <div className="form-group">
+                        <label htmlFor="name">Caster Name</label>
+                        <input className="form-control" id="name" name="name" required />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="pic">Caster Picture</label>
+                        <input className="form-control" id="pic" name="pic" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="city">City</label>
+                        <input className="form-control" id="city" name="city" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="state">State</label>
+                        <input className="form-control" id="state" name="state" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="cuisines">Techniques</label>
+                        <input className="form-control" id="techniques" name="techniques" required />
+                    </div>
+                        <input className="btn btn-primary" type="submit" value="Add Caster" />
+                        </form>
             </main>
         </Def>
     )
